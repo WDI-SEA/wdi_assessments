cafbab035a5f8f704fe39f02afb93d2481c2dbe6
Eryoung-610 <eryoung610@gmail.com>
2020-06-17 14:44:46 -0700

    Assessment finished

---
+++ project-1-assessment/js/main.js
@@ -0,0 +1,28 @@
+let count = document.getElementById("counter");
+let plus = document.getElementById("plus");
+let minus = document.getElementById("minus");
+let userInput = document.getElementById("input");
+let value = 0;
+
+
+plus.addEventListener("click",add);
+minus.addEventListener("click",subtract);
+
+function add(){
+    value += parseInt(userInput.value);
+    count.innerText = value;
+
+    if (value < 0){
+        count.style.color = "red";
+    }
+}
+
+function subtract(){
+    value -= parseInt(userInput.value);
+    count.innerText = value;
+
+    
+    if (value < 0){
+        count.style.color = "red";
+    }
+}

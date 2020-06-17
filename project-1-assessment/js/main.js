6be34c2c29e276dae1d8308934144ef11e3fee1c
Jameson Aardalen <jamesonaardalen@Jamesons-MBP.attlocal.net>
2020-06-17 14:53:59 -0700

    finsished MVP

--- project-1-assessment/js/main.js
+++ project-1-assessment/js/main.js
@@ -8,22 +8,19 @@ window.addEventListener("DOMContentLoaded", (event) => {
   let addBtn = document.getElementById("add");
   let subtractBtn = document.getElementById("subtract");
   let countDisplay = document.getElementById("counter");
+  let increment = document.getElementById("increment");
 
   countDisplay.innerText = `${counter}`;
 
   addBtn.addEventListener("click", () => {
-    counter += 1;
+    counter += parseInt(increment.value);
     // console.log(counter);
     countDisplay.innerText = `${counter}`;
   });
 
   subtractBtn.addEventListener("click", () => {
-    if (counter === 0) {
-      return;
-    } else {
-      counter -= 1;
-      // console.log(counter);
-      countDisplay.innerText = `${counter}`;
-    }
+    counter -= parseInt(increment.value);
+    // console.log(counter);
+    countDisplay.innerText = `${counter}`;
   });
 });

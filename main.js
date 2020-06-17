ad9541dd8e991fdd5fd6b5887dc919824c0989b7
jdephil <jennifer.dephillips@gmail.com>
2020-06-17 18:53:21 -0400

    js done

--- main.js
+++ main.js
@@ -9,26 +9,33 @@ let clickCount = 0
 
 minusBtn.addEventListener('click', function(e) {
     let userInput = document.getElementById('numberIn').value
-    resultDiv.innerText = (resultStart - userInput)
     console.log(resultDiv)
-    clickCount++
-    if (clickCount > 1) {
-        resultDiv.innerText = (resultDiv.innerText - userInput)
+    if (clickCount < 1) {
+        resultDiv.innerText = resultStart - userInput
+        console.log(resultDiv)
         clickCount++
+        console.log(clickCount)
+    } else if (clickCount >= 1) {
+        resultDiv.innerText = resultDiv.innerText - userInput
+        clickCount++
+        console.log(clickCount)
     }
 })
 
 
 plusBtn.addEventListener('click', function(e) {
     let userInput = document.getElementById('numberIn').value
-    resultDiv.innerText = (resultStart + userInput)
     console.log(resultDiv)
-    clickCount++
-    if (clickCount > 1) {
-        resultDiv.innerText = (resultDiv.innerText + userInput)
+    if (clickCount < 1) {
+        resultDiv.innerText = resultStart + Number(userInput)
+        console.log(resultDiv)
         clickCount++
+        console.log(clickCount)
+    } else if (clickCount >= 1) {
+        resultDiv.innerText = Number(resultDiv.innerText) + Number(userInput)
+        clickCount++
+        console.log(clickCount)
     }
-
 
 })
 

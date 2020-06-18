22d2bfef1f3f712df9358ca96242a32fd26a0f7c
sinav <sinavameghi@gmail.com>
2020-06-17 17:42:59 -0700

    all done

--- app.js
+++ app.js
@@ -4,25 +4,22 @@ let final = 0;
 
 let input = document.getElementById("in");
 
-let full = parseInt(input.value)
+let full = 0;
 
-
-
-
-
-
-
-
+input.addEventListener("change", update)
+function update(e){
+    full = e.target.value
+}
 
 document.getElementById("add").addEventListener("click", function(e){
-    final = final + full
+    final = final + parseInt(input.value)
     document.getElementById("number").innerHTML = final;
     
     return
 });
 
 document.getElementById("sub").addEventListener("click", function(e){
-     final = final - full;
+     final = final - parseInt(input.value)
      document.getElementById("number").innerHTML = final;
      
      return

8501b1f4e3236b1122b91dae72e90287ccc73085
karlmiche <karlymism@gmail.com>
2020-06-17 18:40:32 -0700

    calculator, NOT counter app

--- counter.js
+++ counter.js
@@ -4,39 +4,35 @@ const minus = document.getElementById('minus');
 const input = document.getElementById("user-num");
 
 //this works only in console
-let x = parseInt(input.value);
-let clickValue = 0;
-let numValue = x + clickValue;
+//declared this as this but there is no value yet
+let numValue = 0;
+let userNum = 0;
 
 //keyup function
 input.addEventListener("keyup", function (){
-    x = parseInt(input.value);
-    numValue = x + clickValue;
-    numDisplay.innerText = numValue;
+    userNum = parseInt(input.value);
 })
 
 //event listeners for the plus and minus buttons
 plus.addEventListener("click", function (){
-    clickValue++;
+    numValue += userNum
+    numDisplay.innerText = numValue;
     if(numValue < 0){
         numDisplay.style.color = "red";
     }else{
         numDisplay.style.color = "black";
     }
-    numValue = x + clickValue;
-    numDisplay.innerText = numValue;
     return numValue;
 })
 
 minus.addEventListener("click", function (){
-    clickValue--;
+    numValue -= userNum;
+    numDisplay.innerText = numValue
     if(numValue < 0){
         numDisplay.style.color = "red";
     }else{
         numDisplay.style.color = "black";
     }
-    numValue = x + clickValue;
-    numDisplay.innerText = numValue;
     return numValue;
 })
 
